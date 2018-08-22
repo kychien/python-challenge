d837c9afe50dccbcb6baf5627bcc544e9b5fef4b
kychien <41803198+kychien@users.noreply.github.com>
2018-08-22 16:48:30 -0400

    Add files via upload
    Added in a function to help with formatting printouts.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,7 +4,7 @@
 ##
 ##  2018 08 22 - Commented in outline of intended structure and setup output file writer.  Added in
 ##      voter counter.  Added in results calculation with percentages.  Added in winner check.  
-##      Added in results printout.
+##      Added in results printout.  Added in a function to help with formatting printouts.
 ###################################################################################################
 
 
@@ -27,6 +27,20 @@ total = len(voters)
 votes = csvfile["Candidate"].value_counts()
 candidates = csvfile["Candidate"].unique()
 
+
+name_len = [len(name) for name in candidates]
+longest = max(name_len)
+################################################# Function to insert spaces to align outputs
+def spacers(num_spaces):
+    spaces = ""
+    if (num_spaces <= 0):
+        return spaces
+    else:
+        for i in range(num_spaces):
+            spaces += " "
+    return spaces
+
+
 ################################################# Determine winner of the election
 highest = 0                                     #  Highest number of votes value
 win_i = 0                                       #  Index of the winner's name in {candidates[]}
@@ -41,8 +55,16 @@ for frame in votes:
 ################################################# Calculate the percentages of the total vote 
 pct_vote = [(str("%.2f" % (line * 100 / total)) + "%") for line in votes]
 
+################################################# Force every member of pct_vote to be 6 chars
+i = 0
+for frame in pct_vote:                          
+    pct_vote[i] = spacers(6 - len(frame)) + frame
+    i += 1
+
+
+
 ################################################# Get file path for output
-o_file = "election_results.txt"                 #  Initial implementation with fied filename
+o_file = "election_results.txt"                 #  Initial implementation with fixed filename
 
 ################################################# Open writer for output of results to file
 with open(o_file, "w") as results:
@@ -53,7 +75,7 @@ with open(o_file, "w") as results:
     out_line.append(f"Total Votes: {total}")
     out_line.append("-------------------------")
     for j in range(len(candidates)):
-        out_line.append(f"{candidates[j]}:  {pct_vote[j]} ({votes[j]})")
+        out_line.append(f"{spacers(longest - name_len[j])}{candidates[j]}:  {pct_vote[j]} ({votes[j]})")
     out_line.append("-------------------------")
     out_line.append(f"Winner: {candidates[win_i]}")
     out_line.append("-------------------------")
